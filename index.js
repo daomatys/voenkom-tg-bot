603d9f4327601b0e19400e23ec621360a9432381
Maxim Dejnew <red41tiger@mail.ru>
2021-06-07 17:50:21 +0700

    Added different regular messages

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ const token = process.env.BOEHKOM_TOKEN;
 const bot = new tgBot(token, {polling: true});
 
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+let regAnswers = [
+  "ЦЫЦ, А ТО В АРМИЮ ЗАБЕРУ",
+  "*DOOR INTENSIFIES*",
+  "О, ЭТО ВЫ УДАЧНО ДОМОЙ ЗАШЛИ",
+  "ОТКРЫВАЙ, ЭТО МАМА ПРИШЛА С ПРОДУКТАМИ",
+  "НА ГОЛОС НЕ ОБРАЩАЙ ВНИМАНИЯ, ЭТО Я ПРОСТЫЛА"
+];
+
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'НУ ЗДАРОВА, СЫНОК, ВОТ ПОВИСТКА!');
+  
+  bot.sendMessage(chatId, `— ${regAnswers[getRandomInt(regAnswers.length)]}!`);
 });
+
+
