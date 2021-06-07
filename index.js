7e6b81490cc8f6463b127eaf4fc97aa0f8ddc28e
Maxim Dejnew <red41tiger@mail.ru>
2021-06-07 22:33:03 +0700

    Added otchislen command

--- index.js
+++ index.js
@@ -8,15 +8,27 @@ const bot = new tgBot(token, {polling: true});
 const getRandomInt = max => Math.floor( Math.random() * Math.floor(max) );
 
 
+let otchislenList = []
+
 let regAnswers = [
   "— ОТКРОЙТЕ, А ТО В АРМИЮ ЗАБЕРУ!",
   "*DOOR INTENSIFIES*",
   "— О, ЭТО ВЫ УДАЧНО ДОМОЙ ЗАШЛИ!",
   "— СЫНОЧКА, ОТКРЫВАЙ, ЭТО МАМА ПРИШЛА С ПРОДУКТАМИ!",
   "— НА ГОЛОС НЕ ОБРАЩАЙ ВНИМАНИЯ, ЭТО Я ПРОСТЫЛА!",
-  "— ДА ОТКРЫВАЙТЕ УЖЕ, ТАМ КОРМЯТ ТРИ РАЗА В ДЕНЬ, А ДНЕМ — СОНЧАС!"
+  "— ДА ОТКРЫВАЙТЕ УЖЕ, ТАМ КОРМЯТ ТРИ РАЗА В ДЕНЬ, А ДНЕМ ВАБЩЕ СОНЧАС!",
+  "— СКОЛЬКО, ГОВОРИШЬ, ММР У ТЕБЯ?"
 ];
 
-//bot.on('message', msg => bot.sendMessage( msg.chat.id, regAnswers[getRandomInt(regAnswers.length)] ) );
+//bot.on('message', msg => bot.sendMessage(msg.chat.id, regAnswers[getRandomInt(regAnswers.length)] ) );
 
-bot.onText(/\/aaaaa/, msg => bot.sendMessage(msg.chat.id, 'RABOTAET, EPTA!'));
+bot.onText(/\/aaaaa/, msg => bot.sendMessage(msg.chat.id, 'ЧЕК!'));
+
+bot.onText(/\/pnh/, msg => bot.sendMessage(msg.chat.id, 'САМ ИДИ НА;%:, ЩЕНОК, ТЫ КОГДА ПОД СЕБЯ ХОДИЛ, Я ЗА РОДИНУ ВАЕВАЛ МЕЖДУ ВЬЕТКОНГОМ И ЧЕРНИГОВЫМ!'))
+
+bot.onText(/\/otchislen/, msg => {
+  bot.sendMessage(msg.chat.id, `АХАХАХАХАХА, УВИДИМСЯ, @${msg.from.username.toUpperCase()}!`);
+  otchislenList.push({name: msg.from.username,  wincounter: 0});
+});
+  
+
