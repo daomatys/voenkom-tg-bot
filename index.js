d151ba306970aa773668b4b47b1e107840aec10b
Maxim Dejnew <red41tiger@mail.ru>
2021-06-08 16:12:27 +0700

    Modified /spisok command 4

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new tgBot(token, {polling: true});
 const getRandomInt = max => Math.floor( Math.random() * Math.floor(max) );
 
 
-let otchislenList = [
+let otchisList = [
   {name: 'balls', count: 2},
   {name: 'kirsosichka', count: 24},
   {name: 'yobaneim', count: 15}
@@ -20,7 +20,15 @@ let regAnswers = [
   "— СЫНОЧКА, ОТКРЫВАЙ, ЭТО МАМА ПРИШЛА С ПРОДУКТАМИ!",
   "— НА ГОЛОС НЕ ОБРАЩАЙ ВНИМАНИЯ, ЭТО Я ПРОСТЫЛА!",
   "— ДА ОТКРЫВАЙТЕ УЖЕ, ТАМ КОРМЯТ ТРИ РАЗА В ДЕНЬ, А ДНЕМ ВАБЩЕ СОНЧАС!",
-  "— СКОЛЬКО, ГОВОРИШЬ, ММР У ТЕБЯ?"
+  "— СКОЛЬКО, ГОВОРИШЬ, ММР У ТЕБЯ?",
+  "— ЗАКРОЙ ГЛАЗА, ОТКРОЙ ДВЕРЬ :)"
+];
+
+let leftAnswers = [
+  "— САМ ИДИ НА;%:, ЩЕНОК, ТЫ КОГДА ПОД СЕБЯ ХОДИЛ, Я ВАЕВАЛ!! МЕЖДУ ВЬЕТКОНГОМ И ЧЕРНИГОВЫМ!",
+  "— Я ЕЩЕ ВЕРНУСЬ!!!",
+  "— ТЫ АБ ЭТОМ ПАЖЕЛЕЕШЬ!",
+  "— НУ, ПОГОДИ У МЕНЯ!",
 ];
 
 
@@ -28,33 +36,35 @@ let regAnswers = [
 
 
 bot.onText(/\/aaaaa/, msg => {
-  const r = getRandomInt(otchislenList.length);
+  const r = getRandomInt(otchisList.length);
   
-  otchislenList[r].count++;
-  bot.sendMessage(msg.chat.id, `${otchislenList[r].name.toUpperCase()}! ВЫ - РЯДОВОЙ!`);
+  otchisList[r].count++;
+  bot.sendMessage(msg.chat.id, `${otchisList[r].name.toUpperCase()}! ВЫ — РЯДОВОЙ!`);
 });
 
 
-bot.onText(/\/pnh/, msg =>
-  bot.sendMessage(msg.chat.id, 'САМ ИДИ НА;%:, ЩЕНОК, ТЫ КОГДА ПОД СЕБЯ ХОДИЛ, Я ЗА РОДИНУ ВАЕВАЛ МЕЖДУ ВЬЕТКОНГОМ И ЧЕРНИГОВЫМ!')
-);
+bot.onText(/\/pnh/, msg => {
+  bot.sendMessage(msg.chat.id,'under conscruction' )
+});
 
 
-bot.onText(/\/otchislen/, msg => {
-  if (otchislenList.some(item => item.name == msg.from.username)) {
+bot.onText(/\/otchis/, msg => {
+  if (otchisList.some(item => item.name == msg.from.username)) {
     bot.sendMessage(msg.chat.id, `ВТОРОЙ РАЗ НЕ ПРИЗЫВАЮТ, БОЛВАН!`);
   } else {
-    otchislenList.push({name: msg.from.username, count: 0});
+    otchisList.push({name: msg.from.username, count: 0});
     bot.sendMessage(msg.chat.id, `АХАХАХАХАХА, УВИДИМСЯ, @${msg.from.username.toUpperCase()}!`);
   }
 });
 
 
 bot.onText(/\/spisok/, msg => {
-  const setCountWord = a => Math.floor(a/10)==1 || a%10<2 || a%10>4 ? `раз` : `раза` ;
-  const otchislenListGen = item => `• ${item.name} — <i>${item.count} ${setCountWord(item.count)}!</i>`;
+  const setCountWord = a => (Math.floor(a / 10) == 1) || (a % 10 < 2) || (a % 10 > 4) ? `раз` : `раза` ;
   
-  bot.sendMessage(msg.chat.id, otchislenList.map(otchislenListGen).join('\n'), {parse_mode: 'HTML'});
+  const otchisListTitle = `ЛИЧНЫЙ СОСТАВ В/Ч 1337\nЗАЩИТИЛ САПОГИ:\n\n`;
+  const otchisListBody = otchisList.map(item => `• ${item.name} — ${item.count} ${setCountWord(item.count)}!`).join('\n');
+  
+  bot.sendMessage(msg.chat.id, '<code>' + otchisListTitle + otchisListBody + '</code>', {parse_mode: 'HTML'});
 });
 
 
