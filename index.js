69c031a9718f205f7e04b96ee62ae911fc1a1bb9
Maxim Dejnew <red41tiger@mail.ru>
2021-06-24 19:46:06 +0700

    Minor codestyle fixes 5

--- index.js
+++ index.js
@@ -57,17 +57,21 @@ bot.onText(/^\/coolstory/, msg => {
 bot.onText(/^[^/]/, msg => {
   const idTable = getChatId(msg);
   
-  dbNarratorNewTabname().run({ tablename: idTable });
+  dbNarratorNewTabname()
+    .run({ tablename: idTable });
   
-  const heComes = dbNarrators()
-    .get({ tablename: idTable })
-    .state;
+  const heComes = 
+    dbNarrators()
+      .get({ tablename: idTable })
+      .state;
   
   if ( heComes ) {
     bot.sendMessage( msg.chat.id, `<i>— ${dbAnyText('answersRegular')}!</i>`, {parse_mode: 'HTML'} );
   }
   if ( !heComes && getRandomInt(1000) <= heSpawnChance ) {
-    dbNarratorSwitchState().run({ tablename: idTable });
+    dbNarratorSwitchState()
+      .run({ tablename: idTable });
+      
     bot.sendMessage( msg.chat.id, `<b>*${dbAnyText('answersSpawn')}*</b>`, {parse_mode: 'HTML'} );
   }
 });
@@ -76,14 +80,18 @@ bot.onText(/^[^/]/, msg => {
 bot.onText(/^\/pnh/, msg => {
   const idTable = getChatId(msg);
   
-  dbNarratorNewTabname().run({ tablename: idTable });
+  dbNarratorNewTabname()
+    .run({ tablename: idTable });
   
-  const heComes = dbNarrators()
-    .get({ tablename: idTable })
-    .state;
+  const heComes = 
+    dbNarrators()
+      .get({ tablename: idTable })
+      .state;
   
   if ( heComes ) {
-    dbNarratorSwitchState().run({ tablename: idTable });
+    dbNarratorSwitchState()
+      .run({ tablename: idTable });
+    
     bot.sendMessage( msg.chat.id, `<i>— ${dbAnyText('answersQuit')}!</i>`, {parse_mode: 'HTML'} );
   }
 });
@@ -94,10 +102,13 @@ bot.onText(/^\/otchislen/, msg => {
   
   const recruitNameOps = msg.from.username;
   const recruitName = recruitNameOps.toUpperCase();
-  const recruitClone = dbRecruitIsClone(msg).get({ name: recruitNameOps });
+  const recruitClone = 
+    dbRecruitIsClone(msg)
+      .get({ name: recruitNameOps });
   
   if ( recruitClone === undefined) {
-    dbRecruitWrite(msg).run({ name: recruitNameOps });
+    dbRecruitWrite(msg)
+      .run({ name: recruitNameOps });
     
     bot.sendMessage( msg.chat.id, `<i>— ${dbAnyText('recruitMarked')}, @${recruitName}!</i>` , {parse_mode: 'HTML'} );
   } else {
@@ -111,21 +122,25 @@ bot.onText(/^\/aaaa/, msg => {
   
   const idTable = getChatId(msg);
   
-  dbNarratorNewTabname().run({ tablename: idTable });
+  dbNarratorNewTabname()
+    .run({ tablename: idTable });
   
   const calledAt = dbNarrators()
     .get({ tablename: idTable })
     .lastcall;
   
   if ( msg.date > calledAt + callCooldown ) {
-    const r = getRandomInt( dbAnyTableLength( idTable ) );
+    const rnd = getRandomInt( dbAnyTableLength( idTable ) );
     const privateName = dbRecruitById(msg)
-      .get({ id: r })
+      .get({ id: rnd })
       .name
       .toUpperCase();
     
-    dbRecruitUpdate(msg).run({ id: r });
-    dbNarratorSetCalltime(msg).run({ tablename: idTable });
+    dbRecruitUpdate(msg)
+      .run({ id: rnd });
+    
+    dbNarratorSetCalltime(msg)
+      .run({ tablename: idTable });
     
     bot.sendMessage( msg.chat.id, `<i>— @${privateName}, ${dbAnyText('privateJoin')}</i>`, {parse_mode: 'HTML'} );
   } else {
